2ac60b689cf9537453934196d5625bf9a2dd8da0
MrGaggio <steruggiu@gmail.com>
2021-10-13 14:41:23 +0100

    inserito ciclo 0/100

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+//Scrivi un programma che esegua un ciclo da 1 a 100
+
+//a ogni operazione appenda un elemento html al container con stile differente a seconda del valore dell'indice per multipli di 3, per multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
+
+
+
+
+
+
+//Scrivi un programma che esegua un ciclo da 1 a 100
+
+for (let i = 0; i <= 100; i++){
+    console.log(i);
+}

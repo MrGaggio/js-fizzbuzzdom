c19e9f1421c3112d2bca9ac5d1924b2acb9224b4
MrGaggio <steruggiu@gmail.com>
2021-10-13 22:47:52 +0100

    dichiarata variabile  box ma non succede nulla con: if (box == fizz) {     box.style.backgroundColor = 'red'

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@
 
 
 let list = document.querySelector(' .list')
+let box = document.querySelector(' .box')
     console.log(list);
 
 
@@ -29,7 +30,7 @@ for (let i = 1; i <= 100; i++){
 //Associre a ogni variabile i rispettivi numeri multipli
 
 const fizz = '3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45,48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99,'
-console.log(multipleTree);
+console.log(fizz);
 
 const buzz = '5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100,'
 
@@ -38,6 +39,12 @@ const fizzbuzz = '15, 30, 45, 60, 75, 90'
 //Se multiplo di 3 - box verde, scritta fizz
 //Se multiplo di 5 - box giallo, scritta buzz
 //Se multiplo di 15 - box rosso, scritta fizzbuzz
+
+if (box == fizz) {
+    box.style.backgroundColor = 'red'
+    console.log(box);
+}
+
 
 
 
